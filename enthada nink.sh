568200100e1bf7219c1d20df409955397ba8a633
sH4dEu <sh4deeu@gmail.com>
2024-12-16 21:16:44 +0530

    Update enthada nink.sh

--- enthada nink.sh
+++ enthada nink.sh
@@ -204,14 +204,12 @@ check_status() {
 banner() {
 	cat <<- EOF
 		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : ${__version__}
+		___|  |__ _____     __| _/____  
+ /  ___/  |  \\__  \   / __ |/ __ \ 
+ \___ \|   Y  \/ __ \_/ /_/ \  ___/ 
+/____  >___|  (____  /\____ |\___  >
+     \/     \/     \/      \/    \/ 
+                                     ${RED}Version : ${__version__}
 
 		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
 	EOF
